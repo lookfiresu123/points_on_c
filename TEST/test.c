7f735d4118653a289ec4c6c1bcc231bd9aa0af57
lookfiresu <804305986@qq.com>
2015-06-01 16:35:40 +0800

    修改gdb手册，增加TEST目录

---
+++ TEST/test.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+#if 0
+    printf("%d\n",1);  //将字符值0x1赋值到esi寄存器中
+#define f 4  //无任何汇编代码
+#define g "abc"  //只有为"abc"分配内存（文字常量区）的代码
+    printf("%p,%p\n",&g,g);
+    int a = 1;  //讲0x1、0x2、0x5压栈
+    int b = 2;
+    int c = 5;
+    c = a;  //a对应的栈中内容赋值到c对应的栈中
+    printf("%d\n",7);
+    printf("%d\n",c);
+#endif
+#if 0
+    char *p = "abc";//创建栈，并为p、q压栈
+    char *q = "edf";
+#endif
+    char *p;//创建栈，但不为p、q压栈
+    char *q;
+    return 1;
+}
